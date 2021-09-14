edb48557f5e1d3463c010f038c5aabd894fec5af
Jay <Jhgille2@ncsu.edu>
2021-09-14 15:42:25 -0400

    Adding functions to clean lead sheets and sandbox docs

---
+++ R/clean_lead_sheets.R
@@ -0,0 +1,14 @@
+#' A function to read in and tidy data from lead sheets generated with the yield test excel macro.
+#'
+#' @title Tidy lead sheet file content into a list of tibbles.
+#' @param files A vector of file paths to lead sheet excel files.
+#' \value{
+#' A named list of tibbles.
+#' }
+clean_lead_sheets <- function(files = NULL) {
+
+  NULL
+
+}
+
+
