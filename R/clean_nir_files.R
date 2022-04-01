d6d4e4ceda2eb2cba3bd8aa3a157b3cdefd1711b
Jay Gillenwater <jhgille2@ncsu.edu>
2022-04-01 11:46:15 -0400

    Added an option to include the fatty acid columns in the NIR cleaning function

--- R/clean_nir_files.R
+++ R/clean_nir_files.R
@@ -5,7 +5,8 @@
 #' @title A function to clean NIR export files
 #' @param files A vector of file paths to nir csv export files.
 #' @param nir_masterfile The file path the nir masterfile
-clean_nir_files <- function(files = NULL, nir_masterfile = NULL) {
+#' @param select_FA Logical, should fatty acid columns be included in the data
+clean_nir_files <- function(files = NULL, nir_masterfile = NULL, select_FA = FALSE) {
 
   # Read in the nir masterfile
   masterfile <- readr::read_csv(nir_masterfile)
@@ -15,13 +16,41 @@ clean_nir_files <- function(files = NULL, nir_masterfile = NULL) {
   {
     nir_df <- readxl::read_excel(file) %>%
       janitor::clean_names() %>%
-      dplyr::mutate(nir_number_extracted = toupper(stringr::str_extract(sample_id, stringr::regex("nir-[0-9]+", ignore_case = TRUE)))) %>%
-      dplyr::select(sample_id, date_time_of_analysis, predicted_moisture_percent, predicted_protein_dry_basis_percent, predicted_oil_dry_basis_percent, nir_number_extracted) %>%
-      tidyr::separate(sample_id, into = c("year", "loc", "test", "code", "genotype", "rep", "nir_no"), sep = "_") %>%
-      dplyr::mutate(nir_no = nir_number_extracted) %>%
-      dplyr::rename(moisture = predicted_moisture_percent,
-                    oil_dry_basis = predicted_oil_dry_basis_percent,
-                    protein_dry_basis = predicted_protein_dry_basis_percent) %>%
+      dplyr::mutate(nir_number_extracted = toupper(stringr::str_extract(sample_id, stringr::regex("nir-[0-9]+", ignore_case = TRUE))))
+
+    if(select_FA){
+      nir_df %<>%
+        dplyr::select(sample_id,
+                      date_time_of_analysis,
+                      predicted_moisture_percent,
+                      predicted_protein_dry_basis_percent,
+                      predicted_oil_dry_basis_percent,
+                      predicted_linoleic_acid_dry_basis_percent,
+                      predicted_linolenic_acid_dry_basis_percent,
+                      predicted_oleic_acid_dry_basis_percent,
+                      predicted_palmitic_acid_dry_basis_percent,
+                      predicted_stearic_acid_dry_basis_percent,
+                      nir_number_extracted) %>%
+        dplyr::rename(moisture                 = predicted_moisture_percent,
+                      oil_dry_basis            = predicted_oil_dry_basis_percent,
+                      protein_dry_basis        = predicted_protein_dry_basis_percent,
+                      linoleic_acid_dry_basis  = predicted_linoleic_acid_dry_basis_percent,
+                      linolenic_acid_dry_basis = predicted_linolenic_acid_dry_basis_percent,
+                      oleic_acid_dry_basis     = predicted_oleic_acid_dry_basis_percent,
+                      palmitic_acid_dry_basis  = predicted_palmitic_acid_dry_basis_percent,
+                      stearic_acid_dry_basis   = predicted_stearic_acid_dry_basis_percent)
+
+    }else{
+      nir_df %<>%
+        dplyr::select(sample_id, date_time_of_analysis, predicted_moisture_percent, predicted_protein_dry_basis_percent, predicted_oil_dry_basis_percent, nir_number_extracted) %>%
+        tidyr::separate(sample_id, into = c("year", "loc", "test", "code", "genotype", "rep", "nir_no"), sep = "_") %>%
+        dplyr::mutate(nir_no = nir_number_extracted) %>%
+        dplyr::rename(moisture = predicted_moisture_percent,
+                      oil_dry_basis = predicted_oil_dry_basis_percent,
+                      protein_dry_basis = predicted_protein_dry_basis_percent)
+    }
+
+    nir_df %<>%
       dplyr::mutate(year = ifelse(stringr::str_detect(year, stringr::regex("nir", ignore.case = TRUE)), NA, year)) %>%
       dplyr::select(nir_no, date_time_of_analysis, moisture, oil_dry_basis, protein_dry_basis) %>%
       dplyr::left_join(nir_lookup, by = c("nir_no" = "NIR_No")) %>%
