0bcc971455f3e0b6e809e5afe114f1520f2222ca
Jay Gillenwater <jhgille2@ncsu.edu>
2022-04-08 09:01:25 -0400

    Fixed some problems with the protein meal function documentation

--- R/calc_protein_meal.R
+++ R/calc_protein_meal.R
@@ -1,10 +1,10 @@
 #' This function takes the seed protein and seed oil content at a 13% moisture basis and
 #' returns the meal protein content.
 #'
-#' @title Calculate protein meal
-#' @param oil_13_percent The seed oil content at 13% moisture basis
-#' @param protein_13_percent The seed protein content at 13% moisture basis
-#' @references Pantalone, V., & Smallwood, C. (2018) Registration of ‘TN11-5102’ soybean cultivar with high yield and high protein meal. Journal of Plant Registrations, 12:304–308
+#' @title Calculate protein meal.
+#' @param oil_13_percent The seed oil content at 13\% moisture basis.
+#' @param protein_13_percent The seed protein content at 13\% moisture basis.
+#' @references Pantalone, V., & Smallwood, C. (2018) Registration of 'TN11-5102' soybean cultivar with high yield and high protein meal. Journal of Plant Registrations, 12:304-308
 #' @return Meal protein content
 calc_protein_meal <- function(oil_13_percent = NULL, protein_13_percent = NULL) {
 
